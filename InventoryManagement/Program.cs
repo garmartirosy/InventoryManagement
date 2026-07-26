541ab19d5e45dea0cc579348d08cca40fe46105e
gar-mart <garmartirosy@gmail.com>
2026-07-26 01:34:00 -0400

    +

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -36,6 +36,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
